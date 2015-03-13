3b0a71ddb2bc219c96f852b905246c8040123773
Dániel Székely <daniel.sz.wm@gmail.com>
2015-03-13 14:02:26 +0100

    Prepare embed

--- js/iframe-main-v1.1.js
+++ js/iframe-main-v1.1.js
@@ -1,3 +1,7 @@
+// redirect if not viewed in iframe /commented out for testing/
+//if(self.location==top.location)
+//  top.location.replace('http://watchaware.com');
+
 // stop loading indicator
 
 $(window).load(function(){
